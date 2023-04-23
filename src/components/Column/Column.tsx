041c4bce3f0cf054a5083fad6d02fc0b616a8388
nikachu404 <yasinskaa.veronika@gmail.com>
2023-04-23 23:54:46 +0300

    made more appealing column responsiveness

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -14,8 +14,13 @@ export const Column: React.FC<Props> = memo(({ column }) => {
   return (
     <Droppable droppableId={column.id} key={column.id}>
       {(provided) => (
-        <Col {...provided.droppableProps} ref={provided.innerRef}>
+        <Col
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          className="col-sm-6 col-md-6 col-lg-4"
+        >
           <h1 className="text-center">{column.title}</h1>
+
           <div className="column mx-3">
             {column.issues && column.issues.map((issue, index) => (
               <Issue issue={issue} index={index} key={issue.id} />
